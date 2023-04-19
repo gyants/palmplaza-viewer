1876f826fdd374444536223df8b5742545689c19
Giant <isiri.sriuthai@gmail.com>
2023-04-19 16:12:40 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,14 @@
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage, FlexSendMessage
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot import (
+    LineBotApi, WebhookHandler, WebhookParser
+)
+from flask_ngrok import run_with_ngrok
+from flask import Flask, request, abort
 import requests
 import json
 from utils.web_scraper import get_topics, read_replies
@@ -8,18 +19,6 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-from flask import Flask, request, abort
-from flask_ngrok import run_with_ngrok
-
-from linebot import (
-    LineBotApi, WebhookHandler, WebhookParser
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, FlexSendMessage
-)
 
 app = Flask(__name__)
 run_with_ngrok(app)
@@ -28,6 +27,7 @@ CHANNEL_SECRET = os.getenv("CHANNEL_SECRET")
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 parser = WebhookParser(CHANNEL_SECRET)
+
 
 @app.route("/webhook", methods=['POST'])
 def callback():
@@ -42,18 +42,19 @@ def callback():
         events = parser.parse(body, signature)
     except InvalidSignatureError:
         abort(400)
-    
+
     for event in events:
         if event.type == 'message':
-            
+
             if event.message.text.startswith('read'):
                 identifier = event.message.text.split('read')[1].strip()
                 replies = read_replies(identifier)
+                replies = sort_by(replies, 'number')
                 carousel = reply_carousel(replies)
                 line_bot_api.reply_message(
-                event.reply_token,
-                FlexSendMessage(alt_text='Replies',
-                                contents=carousel)
+                    event.reply_token,
+                    FlexSendMessage(alt_text='Replies',
+                                    contents=carousel)
                 )
             else:
                 all_topics = get_topics()
@@ -63,20 +64,21 @@ def callback():
                     FlexSendMessage(alt_text='Forum',
                                     contents=carousel)
                 )
-        
+
         if event.type == 'postback':
             data = event.postback.data.split('&')
             action, identifier = data[0].split('=')[1], data[1].split('=')[1]
             replies = read_replies(identifier)
+            replies = sort_by(replies, 'number')
             carousel = reply_carousel(replies)
             line_bot_api.reply_message(
-            event.reply_token,
-            FlexSendMessage(alt_text='Replies',
-                            contents=carousel)
+                event.reply_token,
+                FlexSendMessage(alt_text='Replies',
+                                contents=carousel)
             )
-        
+
     return 'OK'
+
+
 if __name__ == "__main__":
     app.run()
-
-
